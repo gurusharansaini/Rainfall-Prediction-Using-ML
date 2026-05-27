e4ea5f2ba443694c30e6036fdca414bba4bf03a9
Gurusharansaini <Gurusharansaini4@gmail.com>
2026-05-27 22:16:56 +0530

    change in app.py

--- app.py
+++ app.py
@@ -2,26 +2,27 @@ import streamlit as st
 import pandas as pd
 import model
 # 1. pressure',
-pressure = st.number_input("Enter Pressure", min_value=0, max_value=120)
+pressure = st.number_input("Enter Pressure", min_value=0)
 
 # 2. 'dewpoint'
-dewpoint = st.number_input("Enter Dewpoint", min_value=0, max_value=120)
+dewpoint = st.number_input("Enter Dewpoint", min_value=0)
 
 # 3. 'humidity'
-humidity = st.number_input("Enter Humidity", min_value=0, max_value=120)
+humidity = st.number_input("Enter Humidity", min_value=0)
 
 #4.'cloud'
-cloud = st.number_input("Enter Cloud", min_value=0, max_value=120)
+cloud = st.number_input("Enter Cloud", min_value=0)
 #5.  'sunshine',      
-sushine = st.number_input("Enter Sunshine", min_value=0, max_value=120)
+sushine = st.number_input("Enter Sunshine", min_value=0)
 #6. winddirction
-winddirection = st.number_input("Enter winddirection", min_value=0, max_value=120)
+winddirection = st.number_input("Enter winddirection", min_value=0)
 #7.  'windspeed'
-windspeed = st.number_input("Enter windspeed", min_value=0, max_value=120)
+windspeed = st.number_input("Enter windspeed", min_value=0)
 
 
 print(pressure,dewpoint,cloud)
-input_df = pd.DataFrame([pressure,dewpoint,humidity,cloud,sushine,winddirection,windspeed],columns=model.features_names)
+input_data = (pressure,dewpoint,humidity,cloud,sushine,winddirection,windspeed)
+input_df = pd.DataFrame([input_data],columns=['pressure', 'dewpoint', 'humidity', 'cloud', 'sunshine','winddirection', 'windspeed'])
 predict = model.model.predict(input_df)
 
 if st.button("Predict"):
